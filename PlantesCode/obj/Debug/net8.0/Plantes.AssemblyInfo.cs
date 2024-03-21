3ea1cddfc10ee0c956c36228b7e029038c15fd9e
Asta Martin <martin.asta@hotmail.com>
2024-03-21 19:42:15 +0100

    structure du xunit

--- PlantesCode/obj/Debug/net8.0/Plantes.AssemblyInfo.cs
+++ PlantesCode/obj/Debug/net8.0/Plantes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Plantes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae81a06fddb8850ac427ac1bc44a98d4dbf32bab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Plantes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Plantes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
